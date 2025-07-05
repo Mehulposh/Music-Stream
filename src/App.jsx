5c5eb4d8ad9e7cc69708d4589c4878e30469184a
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-05 19:42:17 +0530

    added search function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { getTracks } from './api/api'
 
 function App() {
   const [data, setData] = useState([]);
+  const [searchedSong, setSearchedSong] = useState([])
   const [loading, setLoading] = useState(false)
   const [currSong, setCurrSong] = useState('');
   
@@ -24,22 +25,42 @@ function App() {
   },[])
   console.log('cursong',currSong)
   return (
-    <div className='bg-blue-400 w-full h-full p-5'>
-      <NavBar />
-      <div className='flex flex-wrap gap-2 mt-3'>
+    <div className='bg-blue-400 w-full h-full p-5 '>
+      <NavBar setData={setSearchedSong}/>
+      <div className='flex flex-wrap gap-2 mt-20 p-5 border border-amber-200 text-center'>
       {
-        data.map(item => (
-          <div key={item["id"]} onClick={() => setCurrSong(item['audio'])}>
-            <img src={item['album_image']} className='rounded-xl h-40'/>
+        searchedSong.length > 0 ? (
+          searchedSong.map(item => (
+          <div 
+            key={item["id"]} 
+            onClick={() => setCurrSong(item['audio'])}
+            className= {`w-[190px] ${currSong === item.audio ? " bg-orange-300" : ""}`}>
+            <img src={item['album_image']} className='rounded-xl size-40'/>
+            <h3 >{item["album_name"]}</h3>
+            <h2>{item['artist_name']}</h2>
+          </div>
+        ))) : (
+          data.map(item => (
+          <div 
+            key={item["id"]} 
+            onClick={() => setCurrSong(item['audio'])}
+            className= {`w-[190px] ${currSong === item.audio ? " p-2 rounded bg-orange-300" : ""}`}
+          >
+            <img src={item['album_image']} className='rounded-xl size-50'/>
             <h3 >{item["album_name"]}</h3>
             <h2>{item['artist_name']}</h2>
           </div>
         ))
+        )
       }
       </div>
 
       <div className='mt-5'>
-        <audio src={currSong} controls autoPlay className='w-full ' />
+        <audio 
+          src={currSong} 
+          controls 
+          autoPlay 
+          className='w-[90%] fixed bottom-10 z-10 ' />
       </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,42 @@
-export  const NavBar = () => {
+import { searchSOng } from "../../api/api"
+import { useState, useEffect } from "react";
+
+export  const NavBar = ({setData}) => {
+    const [query, setQuery] = useState('');
+
+    const handleSearch = async () => {
+        try{
+            const response = await searchSOng(query);
+            setData(response);
+        }catch(err){
+            console.error(err);
+        }
+    };
+
     return (
-        <div className="flex justify-between">
+        <div className="w-[100%] bg-black/75 py-2  flex top-1 rounded bg-blur-sm justify-around items-center fixed bg-blur z-10 ">
             <div>
-                <h3>MP</h3>
+                <h3 className="text-red-500 font-bold text-2xl">MP</h3>
             </div>
-            <input placeholder="Search songs"/>
-            <button>FeedBack</button>
+            <div className="flex gap-4">
+                <input 
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    placeholder="Search songs"
+                    className="bg-white p-2 rounded"
+                />
+                <button 
+                    onClick = {handleSearch} 
+                    className="bg-green-400 p-2 text-white font-semibold rounded"
+                >
+                    Search
+                </button>
+            </div>
+            <button 
+                className="bg-green-400 p-2 text-white font-semibold rounded"
+            >
+                FeedBack
+            </button>
         </div>
     )
 }
