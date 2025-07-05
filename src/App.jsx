62291c06030b9ac315a70d72b70fb78f5e486b8e
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-07-05 17:46:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <audio 
+        src='https://mp3.chillhop.com/serve.php/?mp3=10074'
+        
+      />
+    </div>
+  )
+
+  
+}
+
+export default App
